5e48fb6d646886ac5079a7afc8a9650faf78fc5c
Kilros0817 <lazarkitfrl@gmail.com>
2024-04-14 13:15:12 +0800

    complete testnets

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,18 +1,18 @@
 export const arbitrumId = 421614;
-export const baseId = 8453;
-export const optimismId = 10;
+export const baseId = 84532;
+export const optimismId = 11155420;
 
 
 export const tokenDeployer = {
     [arbitrumId]: "0x573889149441C4F885D4d0cc551BdE613660a383",
-    [baseId]: "0x34f6C5ac104568E98535Cb8c91C5c23AB2d26cAb",
-    [optimismId]: "0x187a9763FCC7EcFFA9bc50B0fE5BbAe71cDA7b59"
+    [baseId]: "0x011cd85cf9b7D32258290e0550305E04e4A48B20",
+    [optimismId]: "0x011cd85cf9b7D32258290e0550305E04e4A48B20"
 }
 
 export const nftMinter = {
     [arbitrumId]: "0xB9106A666A05BF085301D934232aEf15a26650E9",
-    [baseId]: "0x34f6C5ac104568E98535Cb8c91C5c23AB2d26cAb",
-    [optimismId]: "0x187a9763FCC7EcFFA9bc50B0fE5BbAe71cDA7b59"
+    [baseId]: "0xE329Ecca91C8a74EDfC8C60Ed639F1db41b77Cd1",
+    [optimismId]: "0xE329Ecca91C8a74EDfC8C60Ed639F1db41b77Cd1"
 }
 
 export const defaultEthLink = {
